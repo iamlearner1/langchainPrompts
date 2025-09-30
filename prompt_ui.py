c1322065937dfa951d857a91848a9e18601f1aef
Jakka Shiva Kumar Reddy <jakkashivakumar222@gmail.com>
2025-09-30 11:45:43 +0530

    created a dynamic template with there run time variable and new stream lit app with dropdowns and also prompt generator for creating resuable prompts

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,5 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import streamlit as st
+from langchain_core.prompts import PromptTemplate
 llm = HuggingFacePipeline.from_model_id(
     model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
     task='text-generation',
@@ -13,9 +14,30 @@ model = ChatHuggingFace(llm=llm)
 # result = model.invoke("what do you mean by affirmation")
 # print(result)
 
-st.header("Text Summarizer")
-user_input = st.text_input("Enter your prompt ")
+st.header('Reasearch Tool')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = PromptTemplate(
+    template="""
+  Please summarize the research paper titled \"{paper_input}\" with the following specifications:\nExplanation Style: {style_input}  \nExplanation Length: {length_input}  \n1. Mathematical Details:  \n   - Include relevant mathematical equations if present in the paper.  \n   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  \n2. Analogies:  \n   - Use relatable analogies to simplify complex ideas.  \nIf certain information is not available in the paper, respond with: \"Insufficient information available\" instead of guessing.  \nEnsure the summary is clear, accurate, and aligned with the provided style and length.
+""",
+input_variables=['paper_input','style_input','length_input']
+)
+
+prompt = template.invoke({
+
+    'paper_input' : paper_input,
+    'style_input' : style_input,
+    'length_input' : length_input
+})
+
+
 
 if st.button("summarize"):
-    result = model.invoke(user_input)
+    result = model.invoke(prompt)
     st.write(result.content)
