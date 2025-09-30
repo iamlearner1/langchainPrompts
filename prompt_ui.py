988bf2752d3910c700dcd872914ffa4e461fc7eb
Jakka Shiva Kumar Reddy <jakkashivakumar222@gmail.com>
2025-09-30 12:09:34 +0530

    create a resuable template and used in the application

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,6 +1,6 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import load_prompt
 llm = HuggingFacePipeline.from_model_id(
     model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
     task='text-generation',
@@ -22,12 +22,8 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-template = PromptTemplate(
-    template="""
-  Please summarize the research paper titled \"{paper_input}\" with the following specifications:\nExplanation Style: {style_input}  \nExplanation Length: {length_input}  \n1. Mathematical Details:  \n   - Include relevant mathematical equations if present in the paper.  \n   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  \n2. Analogies:  \n   - Use relatable analogies to simplify complex ideas.  \nIf certain information is not available in the paper, respond with: \"Insufficient information available\" instead of guessing.  \nEnsure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['paper_input','style_input','length_input']
-)
+
+template =  load_prompt('template.json')
 
 prompt = template.invoke({
 
